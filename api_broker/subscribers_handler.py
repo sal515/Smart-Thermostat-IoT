f74a682fa120a8521b7d0a033d9c2a62f310c74a
sal515 <salman.rahman515@gmail.com>
2020-04-04 00:58:18 -0400

    Completed basic logic of registerSubscriber method to process registration call

--- api_broker/subscribers_handler.py
+++ api_broker/subscribers_handler.py
@@ -1,18 +1,43 @@
 import database_helper as dbHelper
 import models as models
-
 import json as json
 
-# userInfo = models.deviceInfo(1, "000.000.000.000", "http://127.0.0.1:2000/")
-topic = "data/user_preference"
-# data = dict(userInfo=userInfo.asDict(), topic=topic)
 
-# data = dict(dict(subscribers=[], publishers=[], messages=[]), topic=topic)
-data = dict(models.topic(topic).asDict(), topic=topic)
-dbHelper.json_to_file(data=data, fileName=data["topic"].replace("/", "_"))
+def registerSubscriber(deviceInfo: models.deviceInfo, topic: str):
+    if not dbHelper.isfile(topic):
+        data = dict(models.topic(topic).asDict(), topic=topic)
+        dbHelper.json_to_file(dataObjAsDict=data, topicName=topic)
 
-topic_dictionary = dbHelper.file_to_json(fileName=data["topic"].replace("/", "_"))
-topic_object = models.asTopic(topic_dictionary)
+    topic_dictionary = dbHelper.file_to_json(topic)
+    topic_object = models.asTopic(topic_dictionary)
 
-# def test():
-#     dbHelper.json_to_file(data=data, fileName=topic.replace("/", "_"))
+    # urlMatches = [e for e in topic_object.subscribers if e["deviceURL"] == deviceInfo.deviceURL]
+
+    urlMatches = [a for a in topic_object.subscribers if models.asDeviceInfo(a).deviceURL == deviceInfo.deviceURL]
+
+    # print(urlMatches[0])
+
+    if urlMatches.__len__() > 1:
+        print("Error: Registry issue")
+        return 0, None
+    elif urlMatches.__len__() == 1:
+        print("Already subscribed")
+        return 0, models.asDeviceInfo(urlMatches[0])
+    else:
+        print("Not subscribed, registering user now")
+
+        # generate device id
+        newID = topic_object.subscribers.__len__()
+
+        # set device id
+        deviceInfo.deviceID = newID
+
+        # update object
+        topic_object.subscribers.append(deviceInfo)
+
+        # store device id to file
+        dbHelper.json_to_file(dataObjAsDict=topic_object.asDict(), topicName=topic)
+
+        return 1, deviceInfo
+
+# registerSubscriber()
