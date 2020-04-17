21b19eae8bd44b6e2770e6121fe9e91895504fcc
sal515 <salman.rahman515@gmail.com>
2020-04-17 16:25:58 -0400

    refactored: how we read the fixed header of control_packets

--- test_simpleServer.py
+++ test_simpleServer.py
@@ -12,7 +12,7 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
     """
 
     def handle(self):
-        # self.print_socket_details()
+        self.print_socket_details()
 
         clientAddress = self.client_address
         # self.
@@ -23,97 +23,60 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
         print(self.data)
 
         received_bytes: bytes = self.data
+        received_bytes_list: [] = []
+        remaining_received_bytes_list: [] = []
+
+        for byte in received_bytes:
+            received_bytes_list.append(byte)
+            remaining_received_bytes_list.append(byte)
 
         packet_type = None
         remaining_length_bytes_arr = []
+        remaining_length = -1
 
-        index = 0
-        for b in received_bytes:
+        index = -1
+        for byte in received_bytes_list:
+
+            remaining_received_bytes_list.pop(0)
+            index += 1
+
             if index == 0:
-                print(hex(b))
-                packet_type = b >> 4
-                print(packet_type)
-                if packet_type == 0:
-                    print("Reserved")
-
-                elif packet_type == 1:
-                    print("CONNECT")
-
-                elif packet_type == 2:
-                    print("CONNACK")
-
-                elif packet_type == 3:
-                    print("PUBLISH")
-
-                elif packet_type == 4:
-                    print("PUBACK")
-
-                elif packet_type == 5:
-                    print("PUBREC")
-
-                elif packet_type == 6:
-                    print("PUBREL")
-
-                elif packet_type == 7:
-                    print("PUBCOMP")
-
-                elif packet_type == 8:
-                    print("SUBSCRIBE")
-
-                elif packet_type == 9:
-                    print("SUBACK")
-
-                elif packet_type == 10:
-                    print("UNSUBSCRIBE")
-
-                elif packet_type == 11:
-                    print("UNSUBACK")
-
-                elif packet_type == 12:
-                    print("PINGREQ")
-
-                elif packet_type == 13:
-                    print("PINGRESP")
-
-                elif packet_type == 14:
-                    print("DISCONNECT")
-
-                else:
-                    print("forbidden")
+                # Fixme : Flag bits needs to be taken care of
+                # print(hex(byte))
+                packet_type = byte >> 4
 
             elif index == 1:
-                remaining_length_bytes_arr.append(b)
+                remaining_length_bytes_arr.append(byte)
 
             elif index == 2:
-                remaining_length_bytes_arr.append(b)
+                remaining_length_bytes_arr.append(byte)
 
                 "Remaining Length calculation"
                 multiplier = 1
-                value = 0
+                remaining_length = 0
 
                 # while True:
                 for encodedByte in remaining_length_bytes_arr:
                     try:
-                        # encodedByte = b
-                        value += (encodedByte & 127) * multiplier
+                        # encodedByte = byte
+                        remaining_length += (encodedByte & 127) * multiplier
                         multiplier *= 128
 
                         if multiplier > (128 * 128 * 128):
                             raise ValueError
-
                         if (encodedByte & 128) == 0:
                             break
 
                     except Exception as e:
                         print("Error calculating remaining length: {}".format(e))
                         break
+                # print(remaining_length)
 
-                print(value)
+        self.process_packet(packet_type, remaining_length)
 
             # must be be incremented at the end of the loop
-            print(hex(b))
-            print(index)
-            index += 1
+            # print(hex(byte))
+            # print(index)
 
         # print(type(self.data))
         # received_data: bytes = self.data
@@ -122,6 +85,58 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
 
         # # just send back the same data, but upper-cased
         # self.request.sendall(self.data.upper())
+
+    def process_packet(self, packet_type, remaining_length):
+        print("Packet type: ",  packet_type)
+        print("Remaining length: ",  remaining_length)
+
+        if packet_type == 0:
+            print("Reserved")
+
+        elif packet_type == 1:
+            print("CONNECT")
+
+        elif packet_type == 2:
+            print("CONNACK")
+
+        elif packet_type == 3:
+            print("PUBLISH")
+
+        elif packet_type == 4:
+            print("PUBACK")
+
+        elif packet_type == 5:
+            print("PUBREC")
+
+        elif packet_type == 6:
+            print("PUBREL")
+
+        elif packet_type == 7:
+            print("PUBCOMP")
+
+        elif packet_type == 8:
+            print("SUBSCRIBE")
+
+        elif packet_type == 9:
+            print("SUBACK")
+
+        elif packet_type == 10:
+            print("UNSUBSCRIBE")
+
+        elif packet_type == 11:
+            print("UNSUBACK")
+
+        elif packet_type == 12:
+            print("PINGREQ")
+
+        elif packet_type == 13:
+            print("PINGRESP")
+
+        elif packet_type == 14:
+            print("DISCONNECT")
+
+        else:
+            print("forbidden")
 
     def print_socket_details(self):
         print(self.request)
