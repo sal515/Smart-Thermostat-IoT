996b5903d05e9c5f09668788c4a6d7b241e6c52e
sal515 <salman.rahman515@gmail.com>
2020-04-23 21:59:35 -0400

    Updated some logic of client app ui

--- app_client/ui.py
+++ app_client/ui.py
@@ -7,8 +7,9 @@ def initialization(info_on_file, message):
         dbHelper.json_to_file({}, "user_information")
         info_on_file = False
     message: {} = dbHelper.file_to_json("user_information")
+    message["is_home"] = "0"
     # print(list(message.keys()).__len__())
-    if list(message.keys()).__len__() < 1:
+    if list(message.keys()).__len__() < 2:
         info_on_file = False
 
     return info_on_file, message
@@ -19,12 +20,14 @@ def collect_temperature_preference(message):
     while not message["temperature"].replace(".", "").isdigit():
         message["temperature"] = input(
             "Temperature has to be a float or integer, Please provide temperature value again: : ")
+
     # message["is_home"] = input("Are you currently at home: (yes/no)")
     # while message["is_home"].lower() != "yes" and message["is_home"].lower() != "no":
     #     message["is_home"] = input(
     #         "Presence indication has be a string of either yes or not, please provide your answer again: ")
 
     return message
+
 
 def collect_user_name(message):
     message["user_name"] = input("Please provide your name: ")
